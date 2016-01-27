bcc4c1f854f9dc608bb7100090b2d34bb058fcf7
User <User>
2016-01-27 20:59:41 +0300

    drawing ship & startscreen

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -5,17 +5,17 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.Texture;
 
-/**
- * Created by Toshiba on 17.01.2016.
- */
 public class MainMenuScreen implements Screen{
 
     final Cosmos game;
     OrthographicCamera camera;
 
+
     public MainMenuScreen(final Cosmos gam) {
         game = gam;
+
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 480, 800);
@@ -32,14 +32,14 @@ public class MainMenuScreen implements Screen{
         Gdx.gl.glClearColor(0, 0, 0, 1);
 
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Texture stScr = new Texture("startscreen.png");
 
         camera.update();
 
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
+        game.batch.draw(stScr, camera.position.x - camera.viewportWidth / 2, 0);
 
-        game.font.draw(game.batch, "Kill's all pirates!", 100, 130);
-        game.font.draw(game.batch, "Touch for begin!", 100, 100);
 
         game.batch.end();
 
